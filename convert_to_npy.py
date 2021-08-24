20a231cb7bb2b0f617e2ed28af97832ca435c758
math <mathren90@gmail.com>
2021-08-24 05:25:16 -0400

    standalone script for getSrcCol

---
+++ convert_to_npy.py
@@ -0,0 +1,26 @@
+# author: Mathieu Renzo
+
+# Author: Mathieu Renzo <mathren90@gmail.com>
+# Keywords: files
+
+# Copyright (C) 2019-2021 Mathieu Renzo
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+
+__author__ = "Mathieu Renzo"
+
+from MESAreader import getSrcCol
+import sys
+
+getSrcCol(sys.argv[1], True, True)
