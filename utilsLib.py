f1aa8388d76fbc7384d5065f1c08bb59cc4780a7
math <mathren90@gmail.com>
2021-09-02 22:44:54 +0200

    more output

--- utilsLib.py
+++ utilsLib.py
@@ -246,9 +246,15 @@ def check_and_convert(f, convert=True, terminal_output="out.txt"):
         return
     termination_code = getTerminationCode(f, terminal_output)
     if ((termination_code != "") and \
-        (termination_code != "Couldn't find termination code") and \
-        (termination_code != "min_timestep_limit") and \
-        (termination_code) != "max_model_number"):
+        (termination_code != "Couldn't find termination code")):
+        print(colored("This run did not finish:","yellow"))
+        print(colored(f,"yellow"))
+        return
+    if ((termination_code != "min_timestep_limit") and \
+        (termination_code != "max_model_number")):
+        print(colored("This run failed or reached max_model_number","red"))
+        print(colored(f, "red"))
+    else:
         print("this run finished!")
         if convert:
             src, col = getSrcCol(f+'/LOGS/history.data', convert, convert)
