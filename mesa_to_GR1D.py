ac1f151fc3b653e8583baf8508672bcf982e4f84
math <mathren90@gmail.com>
2022-03-20 13:07:52 -0400

    cleanup and try/except for omega

--- mesa_to_GR1D.py
+++ mesa_to_GR1D.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
-import os
-import array
-import math
 import sys
-import pylab as pl
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import matplotlib.figure as fig
 import numpy as np
 #import scipy as sp
+try:
+    from MESAreader import Msun, Rsun_cm
+    msun = Msun #1.99e33
+    rsun = Rsun_cm # 6.96e10
+except ModuleNotFoundError:
+    print("constants not found in MESAreader")
+    print("manually setting solar mass and radius")
+    mu_sun = 1.3271244e26
+    G = 6.67430e-8
+    msun = mu_sun/G
+    rsun = 6.957e10
 
-msun = 1.99e33
-rsun = 6.96e10
-
-# read the damn thing
+# read profile*.data
 infilename = sys.argv[1]
 outfilename = sys.argv[2]
 modfile = open(infilename,"r")
 profile = modfile.readlines()
 modfile.close()
 
-# okay first three lines are going to be header
+# first three lines are going to be header
 header = [[]]*3
 header[0] = profile[0].split()
 header[1] = profile[1].split()
@@ -28,7 +29,6 @@ header[2] = profile[2].split()
 
 # profile[3] is a blank line
 # stuff starts at profile[4]
-#
 # get column names
 nzones = len(profile[6:])
 colnames = profile[5].split()
@@ -38,18 +38,12 @@ irho = colnames.index("logRho")
 itemp = colnames.index("logT")
 ivel = colnames.index("velocity")
 iye = colnames.index("ye")
-#iomega = colnames.index("omega")
+try:
+    iomega = colnames.index("omega")
+except:
+    pass
 irad = colnames.index("radius")
 imass = colnames.index("mass")
-
-print("zone index: ", izone)
-print("rad index: ", irad)
-print("mass index: ", imass)
-print("rho index: ", irho)
-print("temp index: ", itemp)
-print("vel index: ", ivel)
-print("Y_e index: ", iye)
-#print "Omega index: ", iomega
 
 # data starts at profile[6]
 arr = np.zeros((8,nzones))
@@ -69,7 +63,10 @@ for i in range(nzones):
     # Y_e
     arr[5,i] = float(sline[iye])
     # omega
-    arr[6,i] = 0 #float(sline[iomega])
+    try:
+        arr[6,i] = float(sline[iomega])
+    except:
+        arr[6,i] = 0.0
 
 # radius of cell i is at its outer edge,
 # so are omega, velocity, and mass
